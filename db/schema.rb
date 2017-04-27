722c35a1f1251280778abfbb8a3ca9246c39c3a0
Derrick <whyderrick@gmail.com>
2017-04-26 18:33:54 -0700

    Update schema file (#3)
    I directly updated a migration without running it in PR #1. This commit
    updates the schema file so that it now contains the `unique: true`
    constraint on the User email index.

--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 20170414233058) do
     t.string   "encrypted_password", limit: 128, null: false
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128, null: false
-    t.index ["email"], name: "index_users_on_email", using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
   end
 
