a155f2d983099bd45f3e8700192c4ce058580bc8
Derrick Carr <whyderrick@gmail.com>
2018-04-14 20:53:34 -0700

    Add poems and submissions to the home page
    This commit adds existing submissions for the logged in user to the
    homepage.
    
    If a user has poems, it also  displays them on the homepage. The goal
    here is to make it easier to users to view their existing submissions
    and poems at a glance when they log in.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+    submissions = current_user&.submissions
+    poems = current_user&.poems
 
+    render "index", locals: { submissions: submissions, poems: poems }
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,18 +1,28 @@
 FactoryBot.define do
   factory :user do
-    email "derrick@example.com"
+    sequence(:email) { |n| "user#{n}@example.com" }
     password "derrick"
+
+    factory :user_with_submissions do
+      transient do
+        submission_count 2
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:submission, evaluator.submission_count, user: user)
+      end
+    end
   end
 
   factory :poem do
     user
-    title "The Best Poem"
+    sequence(:title) { |n| "Untitled ##{n}" }
     status "Ready"
   end
 
   factory :submission do
     user
-    title "New Submission"
+    sequence(:title) { |n| "New Submission #{n}" }
     submitted_to "Magnificent Publication"
     status Submission::STATUSES.first
   end
